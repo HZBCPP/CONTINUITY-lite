98b1f45f0e03b60203eea9d3371f3e7f3c05aae9
Elodie <piot.elodie.o@gmail.com>
2021-05-31 11:21:13 -0700

    ENH: Slicer without hard coded paths

--- interface_functions.py
+++ interface_functions.py
@@ -1344,7 +1344,7 @@ class Ui(QtWidgets.QTabWidget):
     # *****************************************
 
     def open_visualisation_button_clicked(self):
-        Ui.run_command("Open visualization interface", [sys.executable, "./CONTINUITY_QC/main_interface_visualization.py", user_json_filename, default_json_filename])
+        Ui.run_command("Open visualization interface", [sys.executable, "./CONTINUITY_QC/main_interface_visualization.py", default_json_filename, user_json_filename])
 
     def open_slicer_first_interface_button_clicked(self):
         Ui.run_command("Open slicer with the first interface", [sys.executable, "./CONTINUITY_QC/slicer_QC.py", user_json_filename])
